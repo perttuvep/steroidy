0c92aa25d04361cdd85acb768dfe7c1edf1777ff
perttuvep <perttuvep@proton.me>
2024-12-28 13:24:27 +0200

    delta time woo

--- main.py
+++ main.py
@@ -6,14 +6,21 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
+    
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
             screen.fill(color="black")
             pygame.display.flip()
+            
+            # NOTE: separate event handler
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    return  
+                    return
+            dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
